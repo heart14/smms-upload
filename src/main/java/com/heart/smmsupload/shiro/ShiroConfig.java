02009233a5ad326e4b8b07fcccf61ff72d3d5822
liHeart <lwf14@qq.com>
2019-03-21 17:51:46 +0800

    add .sql file

---
+++ src/main/java/com/heart/smmsupload/shiro/ShiroConfig.java
@@ -0,0 +1,9 @@
+package com.heart.smmsupload.shiro;
+
+/**
+ * @Description:
+ * @Author: Heart
+ * @Date: 2019/3/21 10:06
+ */
+public class ShiroConfig {
+}
---
+++ src/main/java/com/heart/smmsupload/shiro/ShiroRealm.java
@@ -0,0 +1,9 @@
+package com.heart.smmsupload.shiro;
+
+/**
+ * @Description:
+ * @Author: Heart
+ * @Date: 2019/3/21 10:07
+ */
+public class ShiroRealm {
+}
