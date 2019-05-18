e995339885c7683c1af69c980388d95e2afd3333
heart14 <lwf14@qq.com>
2019-05-18 19:15:26 +0800

    smms upload

--- src/main/java/com/heart/smmsupload/service/SMMSImageService.java
+++ src/main/java/com/heart/smmsupload/service/SMMSImageService.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface SMMSImageService {
 
     /**
-     * 保存图片上传信息
+     * 保存图片信息 将SM.MS API上传成功后返回的图片信息保存到数据库中
      *
      * @param smmsImage
      * @return
@@ -20,7 +20,7 @@ public interface SMMSImageService {
     int saveSMMSImage(SMMSImage smmsImage);
 
     /**
-     * 根据主键删除图片 物理删除
+     * 根据主键删除图片（物理删除）
      *
      * @param imageId
      * @return
@@ -28,22 +28,14 @@ public interface SMMSImageService {
     int removeSMMSImageByPrimaryKey(Integer imageId);
 
     /**
-     * 修改所有图片信息
-     * 魔法删除所有图片
+     * 修改所有图片信息（魔法删除所有图片）
+     *
      * @return
      */
     int editAllSMMSImageStatus();
 
     /**
-     * 根据主键更新图片信息
-     *
-     * @param smmsImage
-     * @return
-     */
-    int editSMMSImageByPrimaryKey(SMMSImage smmsImage);
-
-    /**
-     * 根据用户id更新图片信息 魔法删除一个用户下的所有图片
+     * 根据用户id更新图片信息（魔法删除一个用户下的所有图片）
      *
      * @param userId
      * @return
@@ -51,11 +43,12 @@ public interface SMMSImageService {
     int editSMMSImageByUserId(Integer userId);
 
     /**
-     * 根据所有用户已上传图片
+     * 根据主键更新图片信息（这个真的是更新而不是删除）
      *
+     * @param smmsImage
      * @return
      */
-    List<SMMSImage> findAllSMMSImage();
+    int editSMMSImageByPrimaryKey(SMMSImage smmsImage);
 
     /**
      * 根据主键查询图片
@@ -64,6 +57,13 @@ public interface SMMSImageService {
      * @return
      */
     SMMSImage findSMMSImageByPrimaryKey(Integer imageId);
+
+    /**
+     * 查询所有用户已上传图片
+     *
+     * @return
+     */
+    List<SMMSImage> findAllSMMSImage();
 
     /**
      * 根据用户id查询图片 查询一个用户下的所有图片
